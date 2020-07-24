b572278e1a9c475b333a18ab0644c2f052bc466e
John Mwashuma <jmwashuma@live.com>
2020-07-24 12:48:38 +0300

    Create route to fib and about components

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import About from './About';
+import Fib from './Fib';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>React, redis, node and docker app</h1>
+          <Link to="/">Home</Link>
+          <Link to="/about">About Page</Link>
+        </header>
+        <Route exact path="/" component={Fib} />
+        <Route path="/about" component={About} />
+      </div>
+    </Router>
   );
 }
 
